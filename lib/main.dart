daacb1664cbd50bf544c5b2e2e5f160a61dd2288
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 00:24:44 +0530

    Wrap the Container() widget inside the Column() widget as its one children

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,27 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(),
+          child: Column(
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+                // For adding the margin from all sides
+                // margin: EdgeInsets.all(20),
+
+                // For adding the margins only for top and bottom of the container
+                // margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 30.0),
+
+                // For adding the margins for each and every side separately.
+                margin: EdgeInsets.fromLTRB(40.0, 50.0, 60.0, 70.0),
+
+                // For adding the margins only for one side of the container.
+                // margin: EdgeInsets.only(right: 50.0),
+                child: Text("Hey There!"),
+              )
+            ],
+          ),
         ),
       ),
     );
