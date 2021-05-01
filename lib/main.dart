6c46ee059c4b41885f55e13bfdf3a5d3b3acba37
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-01 11:47:28 +0530

    Change the background color inside the Scaffold() class to perform the hot reload feature of flutter

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: Container(),
       ),
     );
