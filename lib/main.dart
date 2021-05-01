8bdda105822e193bd0fc02dfa0d20ee7b6d4a68f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-01 18:33:32 +0530

    Comment out some part of the code

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
             // For adding the margins only for top and bottom of the container
             // margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 30.0),
 
-            margin: EdgeInsets.fromLTRB(40.0, 50.0, 60.0, 70.0),
+            // margin: EdgeInsets.fromLTRB(40.0, 50.0, 60.0, 70.0),
             child: Text("Hey There!"),
           ),
         ),
