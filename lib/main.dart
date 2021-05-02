7a3cc21c210aa156a43da3d17e650ad19dd089c7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 19:37:13 +0530

    copy the same previous code as it is

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,30 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               // Alternative and More Appropriate Method is as follows -->
-
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'devansh1352000@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Source',
+                        color: Colors.teal[900],
+                      ),
+                    ),
+                  ],
+                ),
+              )
               // Container(
               //   color: Colors.white,
               //   margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
