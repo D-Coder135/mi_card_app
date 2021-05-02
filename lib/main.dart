823116500ebd6fd80176c163c2d117450d1962cf
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 19:16:25 +0530

    add Icon() widget as a child of the Row() widget inside which add a email icon

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,11 @@ class MyApp extends StatelessWidget {
               ),
               Container(
                 child: Row(
-                  children: <Widget>[],
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                    ),
+                  ],
                 ),
               ),
             ],
