b190e98240e7109b3b1551736e59da55bb5bfdb2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 20:00:36 +0530

    set mainAxisAlignment for the Column() widget to center

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
