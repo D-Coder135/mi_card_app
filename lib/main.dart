4dcaa275e6a9a91e1275a561c338b4c3ccfd3527
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 19:35:47 +0530

    Comment out some part of the code

--- lib/main.dart
+++ lib/main.dart
@@ -59,30 +59,31 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
-              Container(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      'devansh1352000@gmail.com',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontFamily: 'Source',
-                        color: Colors.teal[900],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+
+              // Container(
+              //   color: Colors.white,
+              //   margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              //   padding: EdgeInsets.all(10.0),
+              //   child: Row(
+              //     children: <Widget>[
+              //       Icon(
+              //         Icons.email,
+              //         color: Colors.teal,
+              //       ),
+              //       SizedBox(
+              //         width: 10.0,
+              //       ),
+              //       Text(
+              //         'devansh1352000@gmail.com',
+              //         style: TextStyle(
+              //           fontSize: 20.0,
+              //           fontFamily: 'Source',
+              //           color: Colors.teal[900],
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
             ],
             // // mainAxisSize: MainAxisSize.min, // Here it means that the column will occupy min space possible.
             //
